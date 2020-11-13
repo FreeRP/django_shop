b538b3995e2ba0404a76f43d3670421f46fbede4
Alexey <math1993@mail.ru>
2020-11-13 13:15:21 +0300

    Add __str__()

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+
 class Clothes(models.Model):
 
-    title = models.CharField(max_length=100, verbose_name='название')
+    name = models.CharField(max_length=100, verbose_name='название')
     price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='цена')
     img_name = models.CharField(max_length=100, verbose_name='название изображения')
 
+    def __str__(self):
+        return self.name
