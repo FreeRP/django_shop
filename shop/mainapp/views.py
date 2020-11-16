64df6f2131f3f2064d813a1e3683acc1f00bb196
Alexey <math1993@mail.ru>
2020-11-16 10:42:19 +0300

    Renamed: render_category to render_chose_category() Add: global value CATEGORY_TITLES

--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 
+from .models import Clothes
+
+
+CATEGORY_TITLES = {'animals': 'Животные',
+                   'games': 'Игры',
+                   'russia': 'Россия'}
 
 def render_mainpage(request):
-    return render(request, 'base.html')
+    context = {
+        'clothes': list(Clothes.objects.using('animals').all()) + \
+                   list(Clothes.objects.using('games').all()) + \
+                   list(Clothes.objects.using('russia').all()),
+        'categories': CATEGORY_TITLES
+    }
+    return render(request, 'mainapp/show_clothes.html', context)
+
+def render_chosen_category(request, **kwargs):
+    return render(request, 'mainapp/show_clothes.html',
+                  context={'chosen_category': CATEGORY_TITLES[kwargs['name']],
+                           'clothes': Clothes.objects.using(kwargs['name']).all(),
+                           'categories': CATEGORY_TITLES})
