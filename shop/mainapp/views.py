09ea0c8e30e4ecc1ad4c0db3d5440d39baf884ee
Alexey <math1993@mail.ru>
2020-11-16 15:14:28 +0300

    Refactor view.py
    Add ITEMS_PER_PAGE, number of displayed objects per page

--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -8,19 +8,24 @@ CATEGORY_TITLES = {'animals': 'Животные',
                    'games': 'Игры',
                    'russia': 'Россия'}
 
+ITEMS_PER_PAGE = 9
+
 def render_mainpage(request):
     items = list(Clothes.objects.using('animals').all()) + \
             list(Clothes.objects.using('games').all()) + \
             list(Clothes.objects.using('russia').all())
     page_obj = get_page_obj_for_page_number(request.GET.get('page', 1),
-                                            9,
+                                            ITEMS_PER_PAGE,
                                             items)
     return render(request, 'mainapp/show_clothes.html',
                   {'page_obj': page_obj,
                    'categories': CATEGORY_TITLES})
 
 def render_chosen_category(request, **kwargs):
+    page_obj = get_page_obj_for_page_number(request.GET.get('page', 1),
+                                            ITEMS_PER_PAGE,
+                                            Clothes.objects.using(kwargs['name']).all())
     return render(request, 'mainapp/show_clothes.html',
                   context={'chosen_category': CATEGORY_TITLES[kwargs['name']],
-                           'clothes': Clothes.objects.using(kwargs['name']).all(),
+                           'page_obj': page_obj,
                            'categories': CATEGORY_TITLES})
