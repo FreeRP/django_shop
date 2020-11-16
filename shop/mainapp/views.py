51cf56b35e84f48380b767d4e853a6f971391f7b
Alexey <math1993@mail.ru>
2020-11-16 14:56:35 +0300

    Refactor get_page_obj_for_page_number()
    Pagination is handling in services.utils.get_page_obj_for_page_number

--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render
 
 from .models import Clothes
-
+from .services.utils import get_page_obj_for_page_number
 
 CATEGORY_TITLES = {'animals': 'Животные',
                    'games': 'Игры',
                    'russia': 'Россия'}
 
 def render_mainpage(request):
+    items = list(Clothes.objects.using('animals').all()) + \
+            list(Clothes.objects.using('games').all()) + \
+            list(Clothes.objects.using('russia').all())
+    page_obj = get_page_obj_for_page_number(request.GET.get('page', 1),
+                                            9,
+                                            items)
     return render(request, 'mainapp/show_clothes.html',
-                  context = {'clothes': list(Clothes.objects.using('animals').all()) + \
-                                        list(Clothes.objects.using('games').all()) + \
-                                        list(Clothes.objects.using('russia').all()),
-                             'categories': CATEGORY_TITLES})
+                  {'page_obj': page_obj,
+                   'categories': CATEGORY_TITLES})
 
 def render_chosen_category(request, **kwargs):
     return render(request, 'mainapp/show_clothes.html',
