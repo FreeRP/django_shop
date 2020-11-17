f2a116565439c11e1b6fbf251f1a3b100e707182
Alexey <math1993@mail.ru>
2020-11-17 13:20:44 +0300

    Add model Category

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
+class Category(models.Model):
+
+    slug = models.CharField(max_length=100, verbose_name='slug')
+    name = models.CharField(max_length=100, verbose_name='название категории')
+    image_name = models.CharField(max_length=100, verbose_name='название изображения')
+
+    def __str__(self):
+        return self.name
+
 
 class Clothes(models.Model):
 
     name = models.CharField(max_length=100, verbose_name='название')
     price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='цена')
     img_name = models.CharField(max_length=100, verbose_name='название изображения')
+    category = models.ForeignKey(Category, blank=False, null=False, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
